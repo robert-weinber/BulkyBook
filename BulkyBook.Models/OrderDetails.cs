f2620aa67e566565790741a627ef60c028360fe4
weinber_robert <robert.weinber@gmail.com>
2021-09-13 19:06:03 +0200

    OrderDetails model and migration

--- BulkyBook.Models/OrderDetails.cs
+++ BulkyBook.Models/OrderDetails.cs
@@ -8,27 +8,21 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Models
 {
-    public class ShoppingCart
+    public class OrderDetails
     {
-        public ShoppingCart()
-        {
-            Count = 1;
-        }
         [Key]
         public int Id { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public OrderHeader OrderHeader { get; set; }
 
-        public string ApplicationUserId { get; set; }
-        [ForeignKey("ApplicationUserId")]
-        public ApplicationUser ApplicationUser { get; set; }
-
+        [Required]
         public int ProductId { get; set; }
         [ForeignKey("ProductId")]
         public Product Product { get; set; }
 
-        [Range(1,1000,ErrorMessage = "Please enter a value between 1 and 10000")]
         public int Count { get; set; }
-
-        [NotMapped]
         public double Price { get; set; }
     }
 }
