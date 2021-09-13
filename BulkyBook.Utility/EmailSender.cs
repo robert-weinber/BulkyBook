ba475996b05b7cc35f9363cdb5d0464a5404021b
weinber_robert <robert.weinber@gmail.com>
2021-09-14 00:28:29 +0200

    add email support

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,23 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        private readonly EmailOptions emailOptions;
+
+        public EmailSender(IOptions<EmailOptions> options)
+        {
+            emailOptions = options.Value;
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            return Execute(emailOptions.SendGridKey, subject, htmlMessage, email);
+        }
+        private Task Execute(string sendGridKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(sendGridKey);
+            var from = new EmailAddress("admin@bulky.com", "Bulky Books");
+            var to = new EmailAddress(email, "End User");
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", message);
+            return client.SendEmailAsync(msg);
         }
     }
 }
