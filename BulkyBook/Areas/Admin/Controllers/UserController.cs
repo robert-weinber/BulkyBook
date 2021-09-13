49cd10bdab4e3bb66a361c50ef4985153d03cabf
weinber_robert <robert.weinber@gmail.com>
2021-09-13 16:39:29 +0200

    user locking

--- BulkyBook/Areas/Admin/Controllers/UserController.cs
+++ BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -49,7 +49,26 @@ namespace BulkyBook.Areas.Admin.Controllers
             return Json(new { data = userList });
         }
 
-        
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if(objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking"});
+            }
+            if(objFromDb.LockoutEnd!=null && objFromDb.LockoutEnd>DateTime.Now)
+            {
+                // user is locked, we unlock
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Locking/Unlocking successful" });
+        }
 
         #endregion
     }
--- BulkyBook/wwwroot/js/user.js
+++ BulkyBook/wwwroot/js/user.js
@@ -15,37 +15,43 @@ function loadDataTable(){
             { "data": "phoneNumber", "width": "15%" },
             { "data": "company.name", "width": "15%" },
             { "data": "role", "width": "15%" },
-            //{
-            //    "data": "id",
-            //    "render": function (data) {
-            //        return `
-            //                <div class="text-center">
-            //                    <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
-            //                        <i class="fas fa-edit"></i>
-            //                    </a>
-            //                    <a onclick=Delete("/Admin/Category/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
-            //                        <i class="fas fa-trash-alt"></i>
-            //                    </a>
-            //                </div>
-            //        `;
-            //    }, "width": "40%"
-            //}
+            {
+                "data": {
+                    id: "id", lockoutEnd: "lockoutEnd"
+                },
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        //user is locked
+                        return `
+                            <div class="text-center">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+                                    <i class="fas fa-lock-open"></i> Unlock
+                                </a>
+                            </div>
+                    `;
+                    } else {
+                        return `
+                            <div class="text-center">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                                    <i class="fas fa-lock"></i> Lock
+                                </a>
+                            </div>
+                    `;
+                    }
+                }, "width": "25%"
+            }
         ]
     });
 }
 
-function Delete(url) {
-    swal({
-        title: "Are you sure you want to Delete?",
-        text: "You will not be able to restore the data!",
-        icon: "warning",
-        buttons: true,
-        dangerMode : true
-    }).then((willDelete) => {
-        if (willDelete) {
+function LockUnlock(id) {    
             $.ajax({
-                type: "DELETE",
-                url: url,
+                type: "POST",
+                url: '/Admin/User/LockUnlock',
+                data: JSON.stringify(id),
+                contentType: 'application/json',
                 success: function (data) {
                     if (data.success) {
                         toastr.success(data.message);
@@ -56,6 +62,4 @@ function Delete(url) {
                     }
                 }
             });
-        }
-    });
 }
