52de3891cfd3f91620d3e42f72d15948885e83b9
weinber_robert <robert.weinber@gmail.com>
2021-09-13 23:31:46 +0200

    Add cart to session

--- BulkyBook.Utility/SD.cs
+++ BulkyBook.Utility/SD.cs
@@ -19,6 +19,8 @@ namespace BulkyBook.Utility
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
 
+        public const string ssShoppingCart = "Shopping Cart Session";
+
         public static double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
         {
             if(quantity<50)
--- BulkyBook/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBook/Areas/Customer/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ using BulkyBook.Models.ViewModels;
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Http;
 
 namespace BulkyBook.Areas.Customer.Controllers
 {
@@ -71,6 +73,13 @@ namespace BulkyBook.Areas.Customer.Controllers
                     _unitOfWork.ShoppingCart.Update(cartFromDb);
                 }
                 _unitOfWork.Save();
+
+                var count = _unitOfWork.ShoppingCart
+                    .GetAll(c => c.ApplicationUserId == CartObject.ApplicationUserId)
+                    .ToList().Count();
+
+                //HttpContext.Session.SetObject(SD.ssShoppingCart, CartObject);
+                HttpContext.Session.SetInt32(SD.ssShoppingCart, count);
 
                 return RedirectToAction(nameof(Index));
             }
