df76fc54d6d65e478552811dd716628b49b84f66
weinber_robert <robert.weinber@gmail.com>
2021-09-17 14:30:41 +0200

    Comment out SendGrid

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
-using SendGrid;
-using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -23,11 +21,12 @@ namespace BulkyBook.Utility
         }
         private Task Execute(string sendGridKey, string subject, string message, string email)
         {
-            var client = new SendGridClient(sendGridKey);
-            var from = new EmailAddress("admin@bulky.com", "Bulky Books");
-            var to = new EmailAddress(email, "End User");
-            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", message);
-            return client.SendEmailAsync(msg);
+            //var client = new SendGridClient(sendGridKey);
+            //var from = new EmailAddress("admin@bulky.com", "Bulky Books");
+            //var to = new EmailAddress(email, "End User");
+            //var msg = MailHelper.CreateSingleEmail(from, to, subject, "", message);
+            //return client.SendEmailAsync(msg);
+            return null;
         }
     }
 }
